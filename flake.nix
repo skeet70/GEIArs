fed16cf282733a748af2ad976ffce09ccd697228
Murph Murphy <murph@clurictec.com>
2023-08-31 13:32:48 -0600

    Flake for linux and attacker.py

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs.pyproject.url = "github:adisbladis/pyproject.nix";
+  inputs.pyproject.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, pyproject, flake-utils }:
+    let
+      inherit (nixpkgs) lib;
+
+      # Loads pyproject.toml into a high-level project representation
+      project = pyproject.lib.project.loadPyproject {
+        # Read & unmarshal pyproject.toml
+        pyproject = lib.importTOML ./pyproject.toml;
+      };
+
+      # Only using x86_64-linux
+      legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ]
+        (system:
+          import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            config.cudaSupport = true;
+          }
+        );
+      pkgs = legacyPackages.x86_64-linux;
+      python = pkgs.python310;
+
+    in
+    {
+
+      # Create a development shell containing dependencies from `pyproject.toml`
+      devShells.x86_64-linux.default =
+        let
+          # Returns a function that can be passed to `python.withPackages`
+          arg = pyproject.lib.renderers.withPackages { inherit python project; };
+
+          # Returns a wrapped environment (virtualenv like) with all our packages
+          pythonEnv = python.withPackages arg;
+
+        in
+        pkgs.mkShell {
+          packages = [
+            pythonEnv
+            pkgs.python310Packages.torch
+          ];
+        };
+
+      # Build our package using `buildPythonPackage
+      packages.x86_64-linux.default =
+        let
+          # Returns an attribute set that can be passed to `buildPythonPackage`.
+          attrs = pyproject.lib.renderers.buildPythonPackage { inherit python project; };
+        in
+        python.pkgs.buildPythonPackage attrs;
+    };
+}
